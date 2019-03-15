b5b39776fcc7e442fd22df88dc3886ee8c27fa73
AlanDecode <zytbuaa1415@gmail.com>
2019-03-15 03:24:45 +0000

    Travis build

---
+++ README.md
@@ -0,0 +1,181 @@
+# Typecho Theme VOID
+
+> 🐒 猴子打字机原理的产物
+
+作为计算机术语时，VOID 的意思是「无类型」。
+
+![](https://raw.githubusercontent.com/AlanDecode/Typecho-Theme-VOID/master/screenshot.png)
+
+[![build status](https://img.shields.io/travis/AlanDecode/Typecho-Theme-VOID/source.svg?style=flat-square)](https://travis-ci.org/AlanDecode/Typecho-Theme-VOID)
+
+## 特性
+
+> 演示站点：[熊猫小A的博客](https://blog.imalan.cn)，介绍文章：[VOID：现在可以公开的情报](https://blog.imalan.cn/archives/247/)。
+
+* 简版与图片版两种首页样式
+* PJAX 无刷新体验
+* AJAX 评论
+* 响应式设计
+* 优秀的可读性
+* 代码高亮
+* MathJax 公式
+* 表情解析
+* 图片排版（可用作相册）
+* 灵活的头图设置
+* 目录解析
+* 够用的后台设置与丰富的高级设置
+* ...
+
+以及其他很多细节，总之用起来还算舒服。我建立了一个示例页面，在这里你可以看到 VOID 对常用写作元素的支持以及一些特色功能演示：[示例页面](https://blog.imalan.cn/archives/194/)。
+
+## 开始使用
+
+1. 下载主题：[发布版](https://github.com/AlanDecode/Typecho-Theme-VOID/releases) | [开发版](https://github.com/AlanDecode/Typecho-Theme-VOID/archive/nightly.zip)
+2. 解压
+3. **把解压后的文件夹重命名为 VOID**
+4. 检查文件夹名是否为 VOID，不是的话改成 VOID
+5. 检查文件夹名是否为 VOID，不是的话改成 VOID
+6. 检查文件夹名是否为 VOID，不是的话改成 VOID
+7. 上传文件夹至站点 /usr/themes 目录下
+8. 后台启用主题
+
+* 可选：将主题 `assets` 文件夹下的 `VOIDCacheRule.js` 复制一份到站点根目录，并在主题设置中启用 Service Worker 缓存。
+* 可选：主题文件夹下 advanceSetting.sample.json 中有一些高级设置，可以看看。
+
+## **常见问题（请务必仔细阅读）**
+
+<details><summary>下载安装后样式不对？</summary><br>
+
+仓库中的是未压缩的源代码，包含大量实际使用中不需要的文件，并且可能无法直接使用。请一定通过这两个链接下载主题：[发布版](https://github.com/AlanDecode/Typecho-Theme-VOID/releases) | [开发版](https://github.com/AlanDecode/Typecho-Theme-VOID/archive/nightly.zip)
+
+</details>
+
+<details><summary>添加归档页面</summary><br>
+
+新建独立页面，自定义模板选择 `Archives`，内容留空。
+
+</details>
+
+<details><summary>添加友情链接</summary><br>
+
+新建独立页面，然后如此书写：
+
+```
+[links]
+[熊猫小A](https://www.imalan.cn)+(https://secure.gravatar.com/avatar/1741a6eef5c824899e347e4afcbaa75d?s=200&r=G&d=)
+[熊猫小A的博客](https://blog.imalan.cn)+(https://secure.gravatar.com/avatar/1741a6eef5c824899e347e4afcbaa75d?s=64&r=G&d=)
+[/links]
+```
+
+文章中、独立页面中都可以通过该语法插入类似的展示块。
+
+</details>
+
+<details><summary>图片排版</summary><br>
+
+在文章中，使用 `[photos][/photos]` 包起来的图片可显示在同一行。例如：
+
+```
+[photos]
+![](https://cdn.imalan.cn/img/post/2018-10-26/IMG_0073.jpeg)
+![](https://cdn.imalan.cn/img/post/2018-10-26/IMG_0053.jpeg)
+[/photos]
+
+[photos]
+![](https://cdn.imalan.cn/img/post/2018-10-26/IMG_0039.jpeg)
+![](https://cdn.imalan.cn/img/post/2018-10-26/IMG_0051.jpeg)
+![](https://cdn.imalan.cn/img/post/2018-10-26/IMG_0005.jpeg)
+[/photos]
+```
+
+</details>
+
+<details><summary>浏览量统计</summary><br>
+
+使用插件：[TePostViews](https://github.com/AlanDecode/TePostViews)
+
+</details>
+
+<details><summary>文章点赞</summary><br>
+
+使用插件：[Like](https://github.com/AlanDecode/Like)
+
+</details>
+
+## 更新
+
+同[开始使用](#开始使用)，区别是你可以直接覆盖主题文件，不禁用主题，这样你的主题设置就不会丢失。
+
+## 开发
+
+<details><summary>展开详情</summary><br>
+
+如果你有不错的想法，可以定制自己的版本。首先你需要准备好 NodeJS 环境，然后 clone 这个 repo：
+
+```bash
+git clone https://github.com/AlanDecode/Typecho-Theme-VOID ./VOID && cd ./VOID
+```
+
+安装依赖：
+
+```bash
+npm install -g gulp
+npm install
+```
+
+然后将依赖打包：
+
+```bash
+gulp dev
+```
+
+你可以使用自己喜欢的方式编译 SCSS，或者使用：
+
+```bash
+gulp sass
+```
+
+监听 SCSS 更改然后实时编译。尽请添加自己想要的功能，满意后就提交代码。然后：
+
+```bash
+gulp build
+```
+
+构建你的主题，生成的主题位于 `./build` 目录下。如果你对自己的更改很满意，**欢迎提出 Pull Request**。
+
+</details>
+
+## 更新日志
+
+**2019-03-14 Version 1.6.4**
+
+* 可设置是否固定导航栏
+* 自定义下拉栏中链接可设置 target 属性
+* 登录时编辑按钮位置移动
+* 优化字体显示
+* 首页风格设置移动至主题设置
+* 整体色调调整
+* 代码块更改为暗色
+* 消灭了一些臭虫
+
+更多：[change-log.md](https://github.com/AlanDecode/Typecho-Theme-VOID/blob/master/change-log.md)
+
+## 鸣谢
+
+### 开源项目
+
+[JQuery](https://github.com/jquery/jquery) | [highlight.js](https://highlightjs.org/) | [MathJax](https://www.mathjax.org/) | [fancyBox](http://fancyapps.com/fancybox/3/) | [bigfoot.js](http://www.bigfootjs.com/) | [OwO](https://github.com/DIYgod/OwO) | [pjax](https://github.com/defunkt/jquery-pjax) | [yue.css](https://github.com/lepture/yue.css) | [tocbot](https://tscanlin.github.io/tocbot/) | [pangu.js](https://github.com/vinta/pangu.js) | [social](https://github.com/lepture/social)
+
+### 其他
+
+[RAW](https://github.com/AlanDecode/Typecho-Theme-RAW) | [Mirages](https://get233.com/archives/mirages-intro.html) | [handsome](https://www.ihewro.com/archives/489/) | [Card](https://blog.shuiba.co/bitcron-theme-card) | [Casper](https://github.com/TryGhost/Casper) | [Typlog](https://typlog.com/)
+
+## 捐助
+
+**如果本项目对你有所帮助，请考虑捐助我**
+
+![谢谢支持](https://wx1.sinaimg.cn/large/0060lm7Tly1g0c4cbi71lj30sc0iv453.jpg)
+
+## License
+
+MIT © [AlanDecode](https://github.com/AlanDecode)
---
+++ functions.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * functions.php
+ * 
+ * 初始化主题
+ * 
+ * @author      熊猫小A
+ * @version     2019-01-15 1.0
+ */
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+?>
+
+<?php
+
+// 看不见错误就是没有错误
+error_reporting(0);
+
+require_once("libs/Utils.php");
+require_once("libs/Contents.php");
+require_once("libs/Comments.php");
+
+Typecho_Plugin::factory('admin/write-post.php')->bottom = array('Utils', 'addButton');
+Typecho_Plugin::factory('admin/write-page.php')->bottom = array('Utils', 'addButton');
+// 为防止友链解析与 Markdown 冲突，重写 Markdown 函数
+Typecho_Plugin::factory('Widget_Abstract_Contents')->markdown = array('Contents','markdown');
+Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = array('Contents','parseContent');
+Typecho_Plugin::factory('Widget_Abstract_Contents')->excerptEx = array('Contents','parseContent');
+
+/**
+ * 主题启用
+ */
+function themeInit(){
+    Helper::options()->commentsAntiSpam = false;
+    Helper::options()->commentsMaxNestingLevels = 999;
+    Helper::options()->commentsOrder = 'DESC';
+}
+
+/**
+ * 主题设置
+ */
+function themeConfig($form) {
+    $defaultBanner=new Typecho_Widget_Helper_Form_Element_Text('defaultBanner', NULL, 'https://i.loli.net/2019/01/16/5c3e0b5c98bfd.jpeg', '默认顶部大图', '可以填写随机图 API。');
+    $form->addInput($defaultBanner);
+    $fancyIndex=new Typecho_Widget_Helper_Form_Element_Select('fancyIndex',array('0'=>'简洁式','1'=>'卡片式'),'0','首页风格','选择首页风格，简洁式或者卡片式。');
+    $form->addInput($fancyIndex);
+    $ajaxIndex=new Typecho_Widget_Helper_Form_Element_Select('ajaxIndex',array('0'=>'分页','1'=>'加载更多'),'0','首页分页样式','选择首页分页样式：普通分页或者加载更多。仅当卡片式首页有效。');
+    $form->addInput($ajaxIndex);
+    $titleinbanner=new Typecho_Widget_Helper_Form_Element_Select('titleinbanner',array('0'=>'否','1'=>'是'),'1','将标题显示在头图中','是否将标题显示在头图中。');
+    $form->addInput($titleinbanner);
+    $indexBannerTitle=new Typecho_Widget_Helper_Form_Element_Text('indexBannerTitle', NULL, '', '首页顶部大图内文字', '你随意，但不建议太长');
+    $form->addInput($indexBannerTitle);
+    $fixHeader=new Typecho_Widget_Helper_Form_Element_Select('fixHeader',array('1'=>'是', '0'=>'否'),'1','固定导航栏','是否固定导航栏');
+    $form->addInput($fixHeader);
+
+    // 高级设置
+    $serifincontent=new Typecho_Widget_Helper_Form_Element_Select('serifincontent',array('0'=>'不启用','1'=>'启用'),'0','在文章内容中启用衬线体','是否对文章内容中的西文字符启用衬线体（Droid Serif），启用后需要多加载 118 KB 的字体文件。');
+    $form->addInput($serifincontent);
+    $lazyload=new Typecho_Widget_Helper_Form_Element_Select('lazyload',array('0'=>'不启用','1'=>'启用'),'0','图片懒加载','是否启用图片懒加载。');
+    $form->addInput($lazyload);
+    $enableMath=new Typecho_Widget_Helper_Form_Element_Select('enableMath',array('0'=>'不启用','1'=>'启用'),'0','启用数学公式解析','是否启用数学公式解析。启用后会多加载 1~2M 的资源。');
+    $form->addInput($enableMath);
+    $head=new Typecho_Widget_Helper_Form_Element_Textarea('head', NULL, '', 'head 标签输出内容', '统计代码等。');
+    $form->addInput($head);
+    $footer=new Typecho_Widget_Helper_Form_Element_Textarea('footer', NULL, '', 'footer 标签输出内容', '备案号等。');
+    $form->addInput($footer);
+    $pjax=new Typecho_Widget_Helper_Form_Element_Select('pjax',array('0'=>'不启用','1'=>'启用'),'0','启用 PJAX (BETA)','是否启用 PJAX。如果你发现站点有点不对劲，又不知道这个选项是啥意思，请关闭此项。');
+    $form->addInput($pjax);
+    $pjaxreload=new Typecho_Widget_Helper_Form_Element_Textarea('pjaxreload', NULL, NULL, 'PJAX 重载函数', '输入要重载的 JS，如果你发现站点有点不对劲，又不知道这个选项是啥意思，请关闭 PJAX 并留空此项。');
+    $form->addInput($pjaxreload);
+    $serviceworker=new Typecho_Widget_Helper_Form_Element_Select('serviceworker',array('0'=>'不启用','1'=>'启用'),'0','Service Worker','是否启用 Service Worker。Service Worker 可以使浏览器主动缓存静态文件，达到加速效果，但是可能导致某些异常。若要启用，请把主题 assets 文件夹下的 VOIDCacheRule.js 复制一份到<b>站点根目录</b>。');
+    $form->addInput($serviceworker);
+
+    // 超高级设置
+    $advance=new Typecho_Widget_Helper_Form_Element_Textarea('advance', NULL, NULL, 超高级设置, '主题中包含一份 advanceSetting.sample.json，自己仿照着写吧。');
+    $form->addInput($advance);
+}
+
+/**
+ * 文章自定义字段
+ */
+function themeFields(Typecho_Widget_Helper_Layout $layout) {
+    $excerpt = new Typecho_Widget_Helper_Form_Element_Textarea('excerpt', NULL, NULL, '文章摘要', '输入自定义摘要。留空自动从文章截取。');
+    $layout->addItem($excerpt);
+    $showfullcontent = new Typecho_Widget_Helper_Form_Element_Select('showfullcontent',array('0'=>'否', '1'=>'是'),'0','在首页显示完整内容','是否在首页展示完整内容。适合比较短的文章。');
+    $layout->addItem($showfullcontent);
+    $banner = new Typecho_Widget_Helper_Form_Element_Text('banner', NULL, NULL, '文章主图', '输入图片URL，该图片会用于主页文章列表的显示。');
+    $layout->addItem($banner);
+    $bannerasheadimg = new Typecho_Widget_Helper_Form_Element_Select('bannerasheadimg',array('1'=>'是','0'=>'否'),'1','主图显示在文章顶部','默认显示。但你可以强行不显示，转而使用默认头图。');
+    $layout->addItem($bannerasheadimg);
+    $bannerascover = new Typecho_Widget_Helper_Form_Element_Select('bannerascover',array('1'=>'是','0'=>'否'),'1','主图作为首页文章封面','默认显示。但你可以强行不显示。');
+    $layout->addItem($bannerascover);
+    $showTOC=new Typecho_Widget_Helper_Form_Element_Select('showTOC',array('0'=>'不显示目录','1'=>'显示目录'),'0','文章目录','是否显示文章目录。');
+    $layout->addItem($showTOC);
+}
+
+$GLOBALS['VOIDSetting'] = Utils::getVOIDSettings();
